f38f0bb95a94d527501c16c0b681cba1ea342c44
russellsherman <russ.sherman@gmail.com>
2017-08-25 15:55:19 -0700

    install script

--- start.sh
+++ start.sh
@@ -3,6 +3,5 @@
 cd /home/ec2-user
 pwd > /tmp/build.out
 ls >> /tmp/build.out
-tar -xf pubgtracker_build.tar.gz
 source venv/bin/activate
-python monitor.py -p ${PUBG_API_KEY} -s ${SLACK_TOKEN} -c ${SLACK_CHANNEL} -e ${PUBG_SEASON} -m ${PLAYERS_MONITORED}
+python monitor.py -p ${PUBG_API_KEtail Y} -s ${SLACK_TOKEN} -c ${SLACK_CHANNEL} -e ${PUBG_SEASON} -m ${PLAYERS_MONITORED}
