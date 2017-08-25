0b6014e04ddc96e15544a3c545d09a6215355a69
russellsherman <russ.sherman@gmail.com>
2017-08-25 15:29:39 -0700

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 sudo yum -y update
-tar -xf pubgtracker_build.tar.gz
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+tar -xf pubgtracker_build.tar.gz
 source venv/bin/activate
 python monitor.py -p ${PUBG_API_KEY} -s ${SLACK_TOKEN} -c ${SLACK_CHANNEL} -e ${PUBG_SEASON} -m ${PLAYERS_MONITORED}
