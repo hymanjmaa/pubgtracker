d025aa54b8fa6795df8e33a3375ccb77449f0cbf
Russell <russ.sherman@gmail.com>
2017-08-29 23:48:27 -0700

    appspecs and start scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
+cd /home/ec2-user
 sudo yum -y update
-sudo yum -y install python35
-sudo yum -y install python35-pip
+sudo yum -y install python35 python35-pip
 sudo pip-3.5 install -r requirements.txt
