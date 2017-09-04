46bff0a185912709998e66cbe6e04e066c77f355
russellsherman <russ.sherman@gmail.com>
2017-09-04 13:37:42 -0400

    fix startup script

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user
 
 export PUBG_API_KEY=`aws ssm get-parameters --names PUBG_API_KEY --region us-west-2 --with-decryption --query 'Parameters[0].Value' --output text`
 export SLACK_TOKEN=`aws ssm get-parameters --names SLACK_TOKEN --region us-west-2 --with-decryption --query 'Parameters[0].Value' --output text`
-python35 monitor.py -p ${PUBG_API_KEY} -s ${SLACK_TOKEN} -c "#pubg" -e "2017-pre4" -m "iabosi,GlochNessMonster,TheKroniuss,SPLASH5,viceversus" > output.log 2>&1 &
+python35 monitor.py -p ${PUBG_API_KEY} -s ${SLACK_TOKEN} -c "#pubg" -m "iabosi,GlochNessMonster,TheKroniuss,SPLASH5,viceversus" > output.log 2>&1 &
