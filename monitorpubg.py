9ad5dcbe2be06a5926bd9da5d850b8cee4f02f84
Russell <russ.sherman@gmail.com>
2017-10-25 00:19:52 -0700

    better monitor message and logging

--- monitorpubg.py
+++ monitorpubg.py
@@ -21,7 +21,9 @@ class PUBGPlayerMonitor:
         self.api = core.PUBGAPI(pubg_api_key)
         # self.player_match_history = self.collect_player_match_history()
         self.player_wins = self.collect_player_wins()
+        self.slack_message(self.slack_channel, "Collected wins for players {0}.")
         self.player_agg_stats = self.collect_player_agg_stats()
+        self.slack_message(self.slack_channel, "Collected aggregate stats for players {0}.")
 
     def get_player_match_history(self, player_handle):
         """
@@ -93,6 +95,7 @@ class PUBGPlayerMonitor:
                                     stat_collection['wins'][region_stat['Match']] = stat
                                 if stat['field'] == 'Kills':
                                     stat_collection['kills'][region_stat['Match']] = stat
+                    print(stat_collection)
                     return stat_collection
             else:
                 print("No win stats for player: {0}".format(player_handle))
