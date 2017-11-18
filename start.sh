a8970aacf118ae332e8413466b5c0d0403113427
Russell <russ.sherman@gmail.com>
2017-11-18 10:50:04 -0800

    turning off while migrating to v2

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd /home/ec2-user
-
-export PUBG_API_KEY=`aws ssm get-parameters --names PUBG_API_KEY --region us-west-2 --with-decryption --query 'Parameters[0].Value' --output text`
-export SLACK_TOKEN=`aws ssm get-parameters --names SLACK_TOKEN --region us-west-2 --with-decryption --query 'Parameters[0].Value' --output text`
-python35 monitor.py -p ${PUBG_API_KEY} -s ${SLACK_TOKEN} -c "#pubg" -m "iabosi,GlochNessMonster,TheKroniuss,SPLASH5,viceversus" > output.log 2>&1 &
+#cd /home/ec2-user
+#
+#export PUBG_API_KEY=`aws ssm get-parameters --names PUBG_API_KEY --region us-west-2 --with-decryption --query 'Parameters[0].Value' --output text`
+#export SLACK_TOKEN=`aws ssm get-parameters --names SLACK_TOKEN --region us-west-2 --with-decryption --query 'Parameters[0].Value' --output text`
+#python35 monitor.py -p ${PUBG_API_KEY} -s ${SLACK_TOKEN} -c "#pubg" -m "iabosi,GlochNessMonster,TheKroniuss,SPLASH5,viceversus" > output.log 2>&1 &
